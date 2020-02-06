c13f5a38ba49240a2e76b2bc5923df67765a9134
moontracer <themoontracer@gmail.com>
2020-02-06 17:39:11 -0500

    cleaned up api caLLs, reworking

--- src/APITest.js
+++ src/APITest.js
@@ -30,11 +30,33 @@ class APITest extends React.Component {
             console.log(this.state.characters);
             console.log(this.state.tags);
             console.log(this.state.events);
+            console.log(this.state.loading);
         })
     }
     render(){
+        var characterNames = [];
         return (
             <div>
+                {/* Map calls a function on every array element, taking said function in as a parameter.
+                 It also creates a new array instead of overwriting data! 
+                 Characters - represents entire array
+                 character - represent array element
+                */}
+                <div>
+                {
+                    this.state.loading ?
+                    <p>Hasn't been loaded yet</p>
+                    :
+                    Object.keys(this.state.characters).map((character, i) => {
+                        characterNames.Push(this.state.characters[character]);
+                        // return (
+                        //     <p key={i}>{this.state.characters[character]}</p>
+                        //     // Why am I not getting the value?
+                        // )
+                        console.log(characterNames);
+                    })
+                }
+                </div>
             </div>
         )
     }
