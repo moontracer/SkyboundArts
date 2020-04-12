3008844eb2df438dd3fc83382479cb9c92c2c759
moontracer <themoontracer@gmail.com>
2020-04-12 10:13:14 -0400

    created skybound results and added proper routing, need to finish

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import SkyboundHome from "./SkyboundHome.js";
+import SkyboundResults from "./SkyboundResults.js";
 import AddVideo from "./AddVideo.js";
 import Report from "./Report.js";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <SkyboundHome />
       <Switch>
+      <Route exact path="/" component={SkyboundResults} />
       <Route exact path="/add" component={AddVideo}/>
       <Route exact path="/report" component={Report}/>
       </Switch>
